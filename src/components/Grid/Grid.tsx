b368dbc9e2b7db3db882f597c64960d434c3d607
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2024-01-08 12:49:12 +0300

    init

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,11 +1,12 @@
 import styles from './Grid.module.scss';
 import React from 'react';
 import {Box} from "@mui/joy";
+import TimeLine from "../TimeLine/TimeLine";
 
 export default function Grid() {
     return <Box className={styles.root}>
         <Box>
-            TimeLine
+            <TimeLine/>
         </Box>
         <Box>
             <Box>Header</Box>
--- src/components/TimeLine/TimeLine.tsx
+++ src/components/TimeLine/TimeLine.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useRecoilValue } from 'recoil';
-import {calendarViewAtom} from "../../store/atoms";
+import {calendarViewAtom, selectDateTimeFromAtom} from "../../store/atoms";
 import styles from './TimeLine.module.scss';
+import {Sheet} from "@mui/joy";
 
 interface TimelineProps {
     // Дополнительные пропсы, если необходимо
@@ -10,27 +11,44 @@ interface TimelineProps {
 const Timeline: React.FC<TimelineProps> = ({ /* Дополнительные пропсы */ }) => {
     // Получаем текущий тип отображения календаря из атома Recoil
     const calendarView = useRecoilValue(calendarViewAtom);
+    const timeFrom = useRecoilValue(selectDateTimeFromAtom);
+
+    const dayArray = Array.from({ length: 24 }, (_, index) => index);
+    const weekArray = Array.from({ length: 24 }, (_, index) => index);
+    const monthArray = Array.from({ length: timeFrom.daysInMonth() }, (_, index) => index + 1);
+    console.log(dayArray);
 
     // Логика для отображения временной шкалы в зависимости от типа отображения
     const renderTimeline = () => {
         switch (calendarView) {
             case 'day':
                 // Логика для отображения временной шкалы дня
-                return <div>Timeline for Day View</div>;
+                    return <>{dayArray.map((item, index) => (
+                        <div key={index} className={styles.timelineItem}>
+                            <div/>
+                            <div className={styles.timelineLabel}><Sheet>{item}:00</Sheet></div>
+                            <div className={styles.timelineLine}></div>
+                        </div>
+                    ))}</>;
             case 'week':
                 // Логика для отображения временной шкалы недели
                 return <div>Timeline for Week View</div>;
             case 'month':
                 // Логика для отображения временной шкалы месяца
-                return <div>Timeline for Month View</div>;
+                return <>{monthArray .map((item, index) => (
+                    <div key={index} className={styles.timelineItem}>
+                        <div/>
+                        <div className={styles.timelineLabel}><Sheet>{item}</Sheet></div>
+                        <div className={styles.timelineLine}></div>
+                    </div>
+                ))}</>;
             default:
                 return null;
         }
     };
 
     return (
-        <div>
-            <h2>Timeline Component</h2>
+        <div className={styles.verticalTimeline}>
             {renderTimeline()}
         </div>
     );
